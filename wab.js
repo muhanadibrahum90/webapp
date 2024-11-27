47d5be5e26db71db0bd6c462162efbb36c7dd42d
muhanadibrahum90 <muhanadibrahum90@gmail.com>
2024-11-27 21:28:57 +0300

    Add files via upload

---
+++ wab.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    document.getElementById("message").innerText = "شكراً لزيارتك!";
+}
+
+
+// وظيفة البحث (مبدئية)
+function searchFunction() {
+    var input = document.getElementById('searchInput').value;
+    alert('بحث عن: ' + input);
+}
